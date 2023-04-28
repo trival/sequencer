6597f538e2083f7ea1db587c66e18cae26ed4edf
Thomas Gorny <thomas.gorny@pm.me>
2023-04-28 22:43:15 +0200

    implement color test

--- src/pages/tests/colors.tsx
+++ src/pages/tests/colors.tsx
@@ -1,11 +1,11 @@
 import { ToneHighlight, ToneValue, chromaticBgColor } from '@/utils/tone-colors'
 
-interface Props {
+interface TestColorProps {
 	color: string
 	value: ToneValue
 }
 
-const TestColor = ({ color, value }: Props) => {
+const TestColor = ({ color, value }: TestColorProps) => {
 	return (
 		<div
 			className="w-20 h-20 m-1 py-3 px-4 rounded-md box-border select-none touch-none text-gray-800"
@@ -18,41 +18,71 @@ const TestColor = ({ color, value }: Props) => {
 	)
 }
 
-const ColorTestPage = () => {
-	const chromaticValues: ToneValue[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-	const circleOfFithsValues: ToneValue[] = [
-		0, 7, 2, 9, 4, 11, 6, 1, 8, 3, 10, 5,
-	]
-
-	const chromNoneColors = chromaticValues.map((value) => ({
-		color: chromaticBgColor({
-			value,
-			highlight: ToneHighlight.None,
-		}),
-		value,
-	}))
-
+const TestColors = ({
+	values,
+	highlight,
+}: {
+	values: ToneValue[]
+	highlight: ToneHighlight
+}) => {
 	return (
-		<div>
-			<h2>Chromatic Scale</h2>
-			<h3>No Highlight</h3>
+		<>
 			<div className="flex">
-				{chromNoneColors.map((c) => (
-					<TestColor {...c} key={c.value} />
+				{values.map((value) => (
+					<TestColor
+						key={value}
+						color={chromaticBgColor({ value, highlight })}
+						value={value}
+					/>
 				))}
 			</div>
 			<div className="flex">
-				{chromNoneColors
-					.map((c) => ({
-						color: c.color,
-						value: ((c.value + 6) % 12) as ToneValue,
-					}))
-					.map((c) => (
-						<TestColor {...c} key={c.value} />
+				{values
+					.map((val) => ((val + 6) % 12) as ToneValue)
+					.map((value) => (
+						<TestColor
+							key={value}
+							color={chromaticBgColor({ value, highlight })}
+							value={value}
+						/>
 					))}
 			</div>
+		</>
+	)
+}
+
+const ColorTestPage = () => {
+	const chromaticValues: ToneValue[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+	const circleOfFifthsValues: ToneValue[] = [
+		0, 7, 2, 9, 4, 11, 6, 1, 8, 3, 10, 5,
+	]
+
+	return (
+		<div>
+			<h2 className="my-6 font-bold">Chromatic Scale</h2>
+			<h3>No Highlight</h3>
+			<TestColors values={chromaticValues} highlight={ToneHighlight.None} />
 			<h3>Soft Highlight</h3>
+			<TestColors values={chromaticValues} highlight={ToneHighlight.Soft} />
 			<h3>Strong Highlight</h3>
+			<TestColors values={chromaticValues} highlight={ToneHighlight.Strong} />
+
+			<h2 className="my-6 font-bold">Circle of Fifths</h2>
+			<h3>No Highlight</h3>
+			<TestColors
+				values={circleOfFifthsValues}
+				highlight={ToneHighlight.None}
+			/>
+			<h3>Soft Highlight</h3>
+			<TestColors
+				values={circleOfFifthsValues}
+				highlight={ToneHighlight.Soft}
+			/>
+			<h3>Strong Highlight</h3>
+			<TestColors
+				values={circleOfFifthsValues}
+				highlight={ToneHighlight.Strong}
+			/>
 		</div>
 	)
 }
